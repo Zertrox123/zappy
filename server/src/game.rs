0343b81683f8bda2e25e8ff7c804385b9e9a6dce
Morgan Brethome <morgan.brethome@epitech.eu>
2026-06-12 18:22:40 +0200

    feat(server): remove question.md as requested Luca

---
+++ server/src/game.rs
@@ -0,0 +1,47 @@
+use std::usize;
+use rand::Rng;
+
+use crate::data::{Entity, EntityId, Map};
+
+#[derive(Clone, Debug)]
+pub struct Game {
+    map: Map,
+    players: Vec<Entity>,
+}
+
+impl Default for Game {
+    fn default() -> Self {
+        Self::new()
+    }
+
+}
+
+impl Game {
+    pub fn new() -> Self {
+        let mut map = Map::new(2,2);
+        map.populate();
+        Game {
+            map: map,
+            players: Vec::new(),
+        }
+    }
+
+    pub fn run_ticks(&mut self) {}
+
+    pub fn add_players(&mut self) -> EntityId {
+        let id = self.players.len();
+        self.players.push(Entity::new_dummy());
+        self.players[id].setId(id);
+        id as EntityId
+    }
+
+    pub fn get_entity(&mut self, id: EntityId) -> Option<Entity> {
+        let size = self.players.len();
+        let id = id as usize;
+
+        if id > size {
+            return None;
+        }
+        None
+    }
+}
